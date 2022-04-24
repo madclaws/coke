08e1eda112a987558029996e997119cc575fb8ac
madclaws <pavanan.anandu63@gmail.com>
2022-04-25 04:04:16 +0530

    feat: expression structs done

--- src/expr.rs
+++ src/expr.rs
@@ -1,13 +1,54 @@
 /// Module contains traits and structs that encompasses all productions of the syntax grammar
 
-pub trait Expr {
+use crate::token::*;
+use crate::token_type::*;
+
+#[allow(dead_code)]
+pub struct Expr {
 
 }
 
 pub struct Binary {
-
+    left: Expr,
+    operator: Token,
+    right: Expr 
 }
 
-impl Expr for Binary {
-    
+impl Binary {
+    pub fn new(left: Expr, operator: Token, right: Expr) -> Binary {
+        Binary{left, operator, right}
+    }
 }
+
+pub struct Grouping {
+    expression: Expr
+}
+
+impl Grouping {
+    pub fn new(expression: Expr) -> Grouping {
+        Grouping{expression}
+    }
+}
+
+pub struct Unary {
+    operator: Token,
+    right: Expr
+}
+
+impl Unary {
+    pub fn new(operator: Token, right: Expr) -> Unary {
+        Unary{operator, right}
+    }
+}
+
+pub struct Literals {
+    value: Literal
+}
+
+impl Literals {
+    pub fn new(value: Literal) -> Literals {
+        Literals{value}
+    }
+}
+
+
