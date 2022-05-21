3148c0b5a63c5a630f981a335350d259f421843d
madclaws <pavanan.anandu63@gmail.com>
2022-05-21 15:59:41 +0530

    feat: added MParser and its functions

--- src/money_parser.rs
+++ src/money_parser.rs
@@ -51,3 +51,34 @@ impl Display for ParseError {
 }
 
 type ParseResult<T> = Result<T, ParseError>;
+
+pub struct MParser<'a> {
+    tokens: Vec<MToken<'a>>,
+    current: u32
+}
+
+impl <'a>MParser<'a> {
+    fn new(tokens: Vec<MToken<'a>>) -> Self {
+        MParser{tokens, current: 0}
+    }
+
+    // Check if we are end of token stream
+    fn is_eof(&self) -> bool {
+        self.current >= self.tokens.len().try_into().unwrap()
+    }
+
+    // Get a token
+    fn peek(&self) -> Option<&MToken<'a>> {
+        self.tokens.get(self.current as usize)
+    }
+
+    // check if current token is same as the token_type
+    fn is_match(&self, token_type: MTokenType) -> bool {
+        !self.is_eof() && self.peek().unwrap().token_type == token_type
+    }
+
+    // consume and inc the current pointer
+    fn advance(&mut self) {
+        self.current += 1;
+    }
+}
